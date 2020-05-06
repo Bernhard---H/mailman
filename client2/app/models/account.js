8b2c937e916e56885fdf96b8e974ba2cf126ecb4
Bernhard Halbartschlager <halbart.bernhard+git@gmail.com>
2020-05-06 15:38:37 +0200

    ember init

---
+++ client2/app/models/account.js
@@ -0,0 +1,10 @@
+import Model,{attr} from '@ember-data/model';
+
+export default class AccountModel extends Model {
+    @attr username;
+    @attr domain;
+    @attr password;
+    @attr("number") quota;
+    @attr("boolean") enabled;
+    @attr("boolean") sendonl;
+}
